81f89ef5b6c873aa2ac4edfba9dd893cbdb0a15d
Mohammed <awmohammed90@gmail.com>
2025-02-17 19:39:26 -0800

    push files

--- run.local.sh
+++ run.local.sh
@@ -1,1 +1,6 @@
-replicate run Mohammad9227/simple-replicate-model:latest --input_text "hello"
+#cog push r8.im/mohammad9227/my-model-1
+# run prediction
+#push model
+cog push r8.im/mohammad9227/my-model-1
+https://replicate.com/mohammad9227/my-model-1
+replicate run mohammad9227/my-model-1 input_text="I am here"
